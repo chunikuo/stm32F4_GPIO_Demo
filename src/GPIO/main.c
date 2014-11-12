14204e23e8046dd7ed79d9a17175189b60ff8df6
chunikuo <chuni.kuo@gmail.com>
2014-11-13 05:12:31 +0800

    Add: demo3 for GPIO
    polling read the input data register and assign to the output data
    register

--- src/GPIO/main.c
+++ src/GPIO/main.c
@@ -3,28 +3,12 @@
 #include "stm32f4xx_syscfg.h"
 #include "demo1.h"
 #include "demo2.h"
+#include "demo3.h"
 
 int main()
 {
 	demo2();
-	//RCC_Configuration();
-	//GPIO_Configuration();
-	//Interrupts_Configuration();
-
-	//GPIO_ToggleBits(GPIOG, GPIO_Pin_14);
-
-	while(1)
-	{
-		//if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) != status)
-		{
-			//GPIO_ToggleBits(GPIOG, GPIO_Pin_13);
-			//GPIOG->ODR = GPIOA->IDR << 13;
-			//GPIOG->ODR ^= GPIO_Pin_13;
-		//	status ^= status;
-		}
-
-
-	}	
+	
 	return 0;
 }
 
